4c71f9632889f30bf92e4dde7bde36d49dec7a7f
Erik Anderson <erikba@odysseus.anderson.name>
2013-05-01 23:00:38 -0700

    dependency cleanup

--- waterfall/stdafx.h
+++ waterfall/stdafx.h
@@ -12,11 +12,7 @@
 #include <windows.h>
 
 // C RunTime Header Files
-#include <stdlib.h>
-#include <malloc.h>
-#include <memory.h>
 #include <tchar.h>
-
 
 // TODO: reference additional headers your program requires here
 #include <assert.h>
